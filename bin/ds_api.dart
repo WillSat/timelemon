f9794e046f5d2199f684e3f638cbc12dbbc8fdbd
WillSat <will.in@outlook.com>
2025-08-03 09:31:00 +0800

    v0.7.3

--- bin/ds_api.dart
+++ bin/ds_api.dart
@@ -4,18 +4,12 @@ import 'package:dio/dio.dart';
 
 import 'utils.dart';
 
-const sysMsg = '''你是一个社会事件分析专家，从热搜条目中选取较有意义的事件并评估其重要性，输出JSON格式：
-{"list":[{"word":<String:提练热词>,"kind":"社会民生","sign":"important"},"desc":<String:解释描述>},...]}
-1.提练热词必须是经过修饰的名词，尽量不出现句子，务必保证表达准确无歧义，不同榜单存在重复，注意合并热词。例如“我国将出现三大暴雨中心”提炼为“三大暴雨中心”、“中美新一轮经贸会谈最新进展”提炼为“中美新一轮经贸会谈”；
-2.`desc`解释描述可以对知乎热榜中相对应的事件进行50字左右准确概括，禁止虚构；标点符号应该齐全，如句末句号；
-3.`kind`分类严谨；`sign`重要性评估准确。甄别剔除娱乐新闻；
-4.`kind`可选：[政治,经济,社会民生,公共安全,文化,科技,其他]；`sign`可选: [important(值得关注),urgent(影响较大事件),critical(战争级别,尽量不使用)]；
-5.条目数量不限。''';
-
-// DeepSeek API Key
-final apiKey = File('in/deepseek-api.key').readAsStringSync();
-
 Future<String> callDeepSeek(String data) async {
+  // prompt
+  final sysMsg = File('in/prompt-system.txt').readAsStringSync();
+  // DeepSeek API Key
+  final apiKey = File('in/deepseek-api.key').readAsStringSync();
+
   final Map? dsRes = await callDeepSeekAPI(
     apiKey,
     sysMsg,
